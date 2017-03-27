f0232367af8878abe0e1044328e72e3d7ec08b1e
Ali Zahedi <alizahedigol@gmail.com>
2017-03-27 19:18:23 +0430

    Add example for AZPickerView

--- AZViewer Example/ViewController.swift
+++ AZViewer Example/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
 
     var popupView: AZPopupView = AZPopupView()
     var checkBoxView: AZCheckBox = AZCheckBox()
-    var pickerView: AZPickerView = AZPickerView(frame: CGRect(x: 0, y: 0, width: 100, height: 200))
+    var pickerView: AZPickerView = AZPickerView(frame: CGRect(x: 0, y: 40, width: UIScreen.main.bounds.width, height: 30))
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +21,7 @@ class ViewController: UIViewController {
 //        self.view.addSubview(self.popupView)
 //        self.view.addSubview(self.checkBoxView)
     
-        self.view.addSubview(self.pickerView)
-        self.pickerView.pickerData = ["Ali", "Jafar", "Hasa"]
+        self.preparePickerView()
     }
     
     override func didReceiveMemoryWarning() {
@@ -34,5 +33,20 @@ class ViewController: UIViewController {
         
     }
 
+    // prepare picker view
+    fileprivate func preparePickerView(){
+        self.view.addSubview(self.pickerView)
+        self.pickerView.delegate = self
+        self.pickerView.data = [["Item 1", "Item 2", "Item 3"], ["Item4", "Item 5"], ["Section 3", "Item 6"]]
+        self.pickerView.separatorSection = " - - "
+        self.pickerView.selected(indexPath: IndexPath(row: 2, section: 0))
+        print("selected index: \(self.pickerView.index)")
+    }
+}
+
+extension ViewController: AZPickerViewDelegate{
+    func aZPickerView(didSelectRow row: Int, inComponent component: Int) {
+        print("change index: \(component) \(row) index: \(self.pickerView.index)")
+    }
 }
 
