ebb18cb30c4bc003434a1589c964e3adc408946b
Ali Zahedi <alizahedigol@gmail.com>
2017-03-27 19:17:13 +0430

    Add AZHeader, Label and view delegate

---
+++ AZViewer/AZLabel.swift
@@ -0,0 +1,41 @@
+//
+//  AZLabel.swift
+//  AZViewer
+//
+//  Created by Ali Zahedi on 1/7/1396 AP.
+//  Copyright © 1396 AP Ali Zahedi. All rights reserved.
+//
+
+import Foundation
+
+public class AZLabel: UILabel{
+    enum VerticalAlignment {
+        case top
+        case middle
+        case bottom
+    }
+    
+    var verticalAlignment : VerticalAlignment = .top {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    override public func textRect(forBounds bounds: CGRect, limitedToNumberOfLines: Int) -> CGRect {
+        let rect = super.textRect(forBounds: bounds, limitedToNumberOfLines: limitedToNumberOfLines)
+        
+            switch verticalAlignment {
+            case .top:
+                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y, width: rect.size.width, height: rect.size.height)
+            case .middle:
+                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y + (bounds.size.height - rect.size.height) / 2, width: rect.size.width, height: rect.size.height)
+            case .bottom:
+                return CGRect(x: self.bounds.size.width - rect.size.width, y: bounds.origin.y + (bounds.size.height - rect.size.height), width: rect.size.width, height: rect.size.height)
+            }
+    }
+    
+    override public func drawText(in rect: CGRect) {
+        let r = self.textRect(forBounds: rect, limitedToNumberOfLines: self.numberOfLines)
+        super.drawText(in: r)
+    }
+}
