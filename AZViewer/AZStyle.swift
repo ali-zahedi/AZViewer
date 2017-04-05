bc00f582e806145e3d5998f240f4a267f7499bbc
Ali Zahedi <alizahedigol@gmail.com>
2017-04-05 19:04:52 +0430

    Public az Style

--- AZViewer/AZStyle.swift
+++ AZViewer/AZStyle.swift
@@ -8,63 +8,67 @@
 
 import Foundation
 
-class AZStyle{
+public class AZStyle{
     
     fileprivate struct Style {
         static let shared = AZStyle()
     }
     
     // MARK: Public
-    static let shared = AZStyle()
+    public static let shared = AZStyle()
     private init() {} //This prevents others from using the default '()' initializer for this class.
     
     
     // MARK: General
-    var sectionGeneralHeight: CGFloat = CGFloat(30)
-    var sectionGeneralConstant: CGFloat = CGFloat(8)
+    public var sectionGeneralHeight: CGFloat = CGFloat(30)
+    public var sectionGeneralConstant: CGFloat = CGFloat(8)
     
     // MARK: Section Headers
-    var sectionHeaderHeight: CGFloat {
+    public var sectionHeaderHeight: CGFloat {
         return sectionGeneralHeight
     }
-    var sectionHeaderTitleFont = UIFont(name: AZFontString.shared.regular, size: UIFont.preferredFont(forTextStyle: UIFontTextStyle.footnote).pointSize)!
-    var sectionHeaderTitleColor = UIColor.white
-    var sectionHeaderBackgroundColor = UIColor(hex: "FF5E3A")
-    var sectionHeaderAlpha: CGFloat = 1.0
+    public var sectionHeaderTitleFont = UIFont(name: AZFontString.shared.regular, size: UIFont.preferredFont(forTextStyle: UIFontTextStyle.footnote).pointSize)!
+    public var sectionHeaderTitleColor = UIColor.white
+    public var sectionHeaderBackgroundColor = UIColor(hex: "FF5E3A")
+    public var sectionHeaderAlpha: CGFloat = 1.0
     
     // MARK: Section Table
-    var sectionTableHeightImage: CGFloat {
+    public var sectionTableHeightImage: CGFloat {
         return self.sectionGeneralHeight
     }
-    var sectionTableDeactiveCornerColor: UIColor = UIColor(hex: "e4e4e4")
-    var sectionTableDeactiveColor: UIColor = UIColor(hex: "c3c3c3")
-    var sectionTableActiveCornerColor: UIColor = UIColor(hex: "04970a")
-    var sectionTableActiveColor: UIColor = UIColor(hex: "ffffff")
-    var sectionTableFontRow: UIFont = UIFont(name: AZFontString.shared.regular, size: UIFont.preferredFont(forTextStyle: UIFontTextStyle.caption1).pointSize)!
+    public var sectionTableDeactiveCornerColor: UIColor = UIColor(hex: "e4e4e4")
+    public var sectionTableDeactiveColor: UIColor = UIColor(hex: "c3c3c3")
+    public var sectionTableActiveCornerColor: UIColor = UIColor(hex: "04970a")
+    public var sectionTableActiveColor: UIColor = UIColor(hex: "ffffff")
+    public var sectionTableFontRow: UIFont = UIFont(name: AZFontString.shared.regular, size: UIFont.preferredFont(forTextStyle: UIFontTextStyle.caption1).pointSize)!
     
     // MARK: Section View
-    var sectionViewBackgroundColor: UIColor = UIColor(hex: "f8f7f4")
-    var sectionViewSeperatorColor: UIColor = UIColor(hex: "e8e6e1")
+    public var sectionViewBackgroundColor: UIColor = UIColor(hex: "f8f7f4")
+    public var sectionViewSeperatorColor: UIColor = UIColor(hex: "e8e6e1")
     
     // MARK: Section Popup
-    var sectionPopupBlurAlpha: CGFloat = CGFloat(0.75)
-    var sectionPopupBlurEffect: UIBlurEffect = UIBlurEffect(style: UIBlurEffectStyle.light)
-    var sectionPopupCornerRadius: CGFloat = CGFloat(5)
-    var sectionPopupMarginMultiplier: CGFloat = CGFloat(0.10)
-    var sectionPopupBackgroundColor: UIColor = UIColor.white
+    public var sectionPopupBlurAlpha: CGFloat = CGFloat(0.75)
+    public var sectionPopupBlurEffect: UIBlurEffect = UIBlurEffect(style: UIBlurEffectStyle.light)
+    public var sectionPopupCornerRadius: CGFloat = CGFloat(5)
+    public var sectionPopupMarginMultiplier: CGFloat = CGFloat(0.10)
+    public var sectionPopupBackgroundColor: UIColor = UIColor.white
     
     // MARK: Section Input
-    var sectionInputFont: UIFont = UIFont(name: AZFontString.shared.regular, size: UIFont.preferredFont(forTextStyle: UIFontTextStyle.body).pointSize)!
-    var sectionInputIconColor: UIColor = UIColor(hex: "e4e4e4")
-    var sectionInputLeftPadding: CGFloat = CGFloat(3)
+    public var sectionInputFont: UIFont = UIFont(name: AZFontString.shared.regular, size: UIFont.preferredFont(forTextStyle: UIFontTextStyle.body).pointSize)!
+    public var sectionInputIconColor: UIColor = UIColor(hex: "e4e4e4")
+    public var sectionInputLeftPadding: CGFloat = CGFloat(3)
     
     // MARK: Section Picker View
-    var sectionPickerViewItemFont: UIFont = UIFont(name: AZFontString.shared.regular, size: UIFont.preferredFont(forTextStyle: UIFontTextStyle.caption1).pointSize)!
-    var sectionPickerViewItemColor: UIColor = UIColor.black
+    public var sectionPickerViewItemFont: UIFont = UIFont(name: AZFontString.shared.regular, size: UIFont.preferredFont(forTextStyle: UIFontTextStyle.caption1).pointSize)!
+    public var sectionPickerViewItemColor: UIColor = UIColor.black
     
-    let availableThemes = ["apple", "lightGray"]
+    // MARK: Section Date Pickerview
+    public var sectionDatePickerViewFormatDate: String = "yyyy/MM/dd"
+    public var sectionDatePickerViewFormatTime: String = "HH:mm:ss"
     
-    func loadTheme(){
+    public let availableThemes = ["apple", "lightGray"]
+    
+    public func loadTheme(){
         
         let defaults = UserDefaults.standard
         if let name = defaults.string(forKey: "AZTheme"){
