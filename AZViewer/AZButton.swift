5327240a1f8e90338b828ec4d193bd689aca1112
Ali Zahedi <alizahedigol@gmail.com>
2017-06-04 11:15:31 +0430

    Add background button for state

--- AZViewer/AZButton.swift
+++ AZViewer/AZButton.swift
@@ -75,6 +75,16 @@ extension AZButton {
         self.removeAnimationButton(sender: self.loaderMask)
         self.removeAnimationLoader()
     }
+
+    public func setBackground(color: UIColor, forState: UIControlState) {
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        UIGraphicsGetCurrentContext()!.setFillColor(color.cgColor)
+        UIGraphicsGetCurrentContext()!.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        self.setBackgroundImage(colorImage, for: forState)
+    }
 }
 
 // animation
